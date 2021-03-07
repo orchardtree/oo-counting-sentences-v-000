e9d023b85ae94c838bf15991587fc8b65d7c8ed0
Jesse Wenger <wengerj@gmail.com>
2021-03-07 03:20:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with?(".")
   end
 
   def question?
-
+    end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
 
   def count_sentences
-
+    self
+    binding.pry
   end
 end
